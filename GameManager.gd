eba07ade7acec1f5931e52d3d97085544f4a75ec
cyanLaStride <66334063+cyanLaStride@users.noreply.github.com>
2023-04-29 19:47:13 -0400

    Game Manager
    dvidberg 2304291946
    added GameManager, added timer and dayStart function, call it to start timer

---
+++ GameManager.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var timerMax = 300.0
+@export var timer = 0.0
+@export var timerOn = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if timerOn:
+		timer -= delta
+		print(timer)
+		if timer < 0:
+			print("DAY END")
+			timerOn = false
+	
+
+func dayStart():
+	timer = timerMax
+	timerOn = true
+
+func _input(_event): # debug, remove at cleanup
+	if Input.is_key_pressed(KEY_T):
+		dayStart()
