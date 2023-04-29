dca95130a41e9aecdb17800d74206e056e05d7f7
watet3953 <106386185+watet3953@users.noreply.github.com>
2023-04-29 17:29:21 -0400

    added more item stuff and collisions and all that yeah

--- classes/Item.gd
+++ classes/Item.gd
@@ -7,6 +7,7 @@ class_name Item
 @export var weight : float = 1.0 # weight in units
 @export var canStun : bool = false # can the item stun a player when thrown
 @export var canTrip : bool = false # can the item knock over a player
+@export var solid : bool = false # if an object will collide with the player while on the ground
 @export var fragile : bool = false # will the item break when thrown?
 
 var thrown : bool = false
@@ -14,27 +15,63 @@ var thrown : bool = false
 var fallDelta : float = 0.0
 
 @onready var collider : CollisionShape2D = $Collider
+@onready var sprite : Sprite2D = $Sprite
 
 func _ready():
-	linear_damp = weight
+	set_collision_layer_value(1, solid)
+	set_collision_mask_value(1, solid)
 
 func _process(delta):
 	if thrown:
 		fallDelta -= delta
+		sprite.global_position = global_position + Vector2(0, -fallDelta * 20)
 		if fallDelta <= 0.0:
 			thrown = false
-			linear_damp = weight
+			linear_damp = 5.0
+			set_collision_layer_value(1, solid)
+			set_collision_mask_value(1, solid)
 			if fragile:
 				queue_free()
-		if fallDelta <= 0.8:
-			collider.disabled = false
+		elif fallDelta <= 0.8:
+			set_collision_layer_value(1, true)
+			set_collision_mask_value(1, true)
 
 func _set_collision(toggle : bool):
-		collider.disabled = !toggle
+	set_collision_layer_value(1,toggle)
+	set_collision_mask_value(1,toggle)
+
+func _catch():
+	thrown = false
+	linear_velocity = Vector2.ZERO
+	linear_damp = 0.0
+	fallDelta = 0.0
+	sprite.global_position = global_position
+	set_collision_layer_value(1, false)
+	set_collision_mask_value(1, false)
 
 func _throw(force : float, direction : Vector2):
 	thrown = true
 	linear_velocity += direction * force * (1 / weight)
 	linear_damp = 0.0
 	fallDelta = 1.0
-	collider.disabled = true
+	sprite.global_position = global_position + Vector2(0, -fallDelta * 20)
+	set_collision_layer_value(1, false)
+	set_collision_mask_value(1, false)
+
+
+func _on_body_entered(body):
+	if body is Entity:
+		if canStun and thrown:
+			body._stun()
+		if canTrip:
+			body._stun()
+	if fragile:
+		queue_free()
+
+
+func _on_interactor_body_entered(body):
+	if body is Entity:
+		if canStun and thrown and get_collision_layer_value(1):
+			body._stun()
+		if canTrip:
+			body._stun()
