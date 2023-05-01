286ea7869a7b6ba0c4e0fd83ce0465bfc7130dca
bonkybub <shotundematthew171@gmail.com>
2023-05-01 04:41:32 -0400

    jdsnjkvkjvsknfv

--- classes/ItemSpawner.gd
+++ classes/ItemSpawner.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 @export var foodItem : FoodItem = null
 @export var item : Item = null
-@export var spawnDelta : int = 0
-@export var lifeDelta : int = 0
+@export var spawnDelta : float = 0
+@export var lifeDelta : float = 0
 
 const NEWFOOD = preload("res://classes/FoodItem.tscn")
 const NEWITEM = preload("res://classes/Item.tscn")
@@ -17,7 +17,7 @@ var foodList = [0, 1, 7, 2, 3, 4, 6, 5]
 
 func _ready():
 	rng.randomize()
-	spawnDelta = rng.randi_range(240, 480)
+	spawnDelta = rng.randi_range(4, 8)
 
 func _process(delta):
 	# timer for item spawning
@@ -36,7 +36,7 @@ func _process(delta):
 	# if item does not spawn
 	if spawnChance == 2:
 		rng.randomize()
-		spawnDelta = rng.randi_range(240, 480)
+		spawnDelta = rng.randi_range(4, 8)
 		spawnChance = 0
 	
 	# coinflip if item is food or not
@@ -54,10 +54,10 @@ func _process(delta):
 		foodItem.global_position = global_position
 		rng.randomize()
 		foodItem.foodArr[foodList[rng.randi_range(0, GameManager.foodMax)]] += 1
+		foodItem.foodCheck()
 		rng.randomize()
-		lifeDelta = rng.randi_range(1200, 1440)
+		lifeDelta = rng.randi_range(20, 24)
 		itemChance = 3
-		print(foodItem.foodArr)
 	# if item is not food, randomize which base item is spawned
 	if itemChance == 2:
 		item = NEWITEM.instantiate()
@@ -68,9 +68,8 @@ func _process(delta):
 		rng.randomize()
 		item.id = rng.randi_range(1, 5)
 		rng.randomize()
-		lifeDelta = rng.randi_range(1200, 1440)
+		lifeDelta = rng.randi_range(20, 24)
 		itemChance = 4
-		print(item.id)
 	
 	# if item spawned, start counting down for life time
 	if lifeDelta > 0:
@@ -79,27 +78,29 @@ func _process(delta):
 	# reset timer if player picks up food item
 	if itemChance == 3 and foodItem.held:
 		rng.randomize()
-		spawnDelta = rng.randi_range(240, 480)
+		spawnDelta = rng.randi_range(4, 8)
 		foodItem = null
 		itemChance = 0
+		lifeDelta = 0
 	# reset timer if player picks up item
 	if itemChance == 4 and item.held:
 		rng.randomize()
-		spawnDelta = rng.randi_range(240, 480)
+		spawnDelta = rng.randi_range(4, 8)
 		item = null
 		itemChance = 0
+		lifeDelta = 0
 	
 	# despawn food item
 	if lifeDelta <= 0 and itemChance == 3 and not foodItem.held:
 		get_parent().remove_child(foodItem)
 		rng.randomize()
-		spawnDelta = rng.randi_range(240, 480)
+		spawnDelta = rng.randi_range(4, 8)
 		foodItem = null
 		itemChance = 0
 	# despawn item
 	if lifeDelta <= 0 and itemChance == 4 and not item.held:
 		get_parent().remove_child(item)
 		rng.randomize()
-		spawnDelta = rng.randi_range(240, 480)
+		spawnDelta = rng.randi_range(4, 8)
 		item = null
 		itemChance = 0
